519654bfc69c59bc58f565d56d60f64d811116e0
Sergio Barreras <sergiobg.isc@gmail.com>
2024-09-03 20:40:56 -0700

    style: math result fittedbox

--- lib/widgets/main_result.dart
+++ lib/widgets/main_result.dart
@@ -11,7 +11,9 @@ class MainResult extends StatelessWidget {
       margin: const EdgeInsets.only(bottom: 20),
       width: double.infinity,
       alignment: Alignment.centerRight,
-      child: Text(text, style: const TextStyle(fontSize: 50)),
+      child: FittedBox(
+          fit: BoxFit.contain,
+          child: Text(text, style: const TextStyle(fontSize: 50))),
     );
   }
 }
